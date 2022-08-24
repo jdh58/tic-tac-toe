203eeccf99dfc0c8fee46739021f9d4c54245c14
Jonathan Hawes <jhawes580@gmail.com>
2022-08-23 22:52:45 -0700

    Update to fully working version
    Known Issues:
    - Messy code
    - No CPU opponent
    - Unable to restart game once a player reaches 5 wins

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const playerFactory = (symbol, name) => {
+const playerFactory = (symbol, name, wins) => {
+    wins = 0;
 
     const getSymbol = () => {
         return symbol;
@@ -6,57 +7,69 @@ const playerFactory = (symbol, name) => {
 
     const getName = () => {
         return name;
+    }
+
+    const getWins = () => {
+        return wins;
     } 
 
-    return {getSymbol, getName};
+    const setWins = (value) => {
+        wins = value;
+    }
+
+    return {getSymbol, getName, getWins, setWins};
 }
 
 const gameBoard = () => {
-    const _gameGrid = [];
+    let _gameGrid = [];
 
-    const _visualSquarePlayed = (target, playerValue) => {
+    const _visualSquarePlayed = (target, playerSymbol) => {
         let targetSquare = document.querySelector(`.boardSquare:nth-child(${target})`);
-        targetSquare.textContent = playerValue;
-
+        targetSquare.textContent = playerSymbol;
     }
     
     const getSquare = (target) => { return _gameGrid[target]; };
-    const setSquare = (target, playerValue) => { 
-        _gameGrid[target] = playerValue;
-        _visualSquarePlayed(target);
-        
+    const setSquare = (target, playerSymbol) => { 
+        _gameGrid[target] = playerSymbol;
+        _visualSquarePlayed(target, playerSymbol);
     };
 
-    const checkWin = (player1, player2) => {
-        if (_gameGrid[1] === _gameGrid[2] === _gameGrid[3] === 1 ||
-        _gameGrid[4] === _gameGrid[5] === _gameGrid[6] === 1 ||
-        _gameGrid[7] === _gameGrid[8] === _gameGrid[9] === 1 ||
-        _gameGrid[1] === _gameGrid[4] === _gameGrid[7] === 1 ||
-        _gameGrid[2] === _gameGrid[5] === _gameGrid[8] === 1 ||
-        _gameGrid[3] === _gameGrid[6] === _gameGrid[9] === 1 ||
-        _gameGrid[1] === _gameGrid[5] === _gameGrid[9] === 1 ||
-        _gameGrid[3] === _gameGrid[5] === _gameGrid[7] === 1 ) {
-            // Increment player1 wins by 1
-            player1.setWins(player1.getWins()+1);
-            return true;
-        } else if (_gameGrid[1] === _gameGrid[2] === _gameGrid[3] === 2 ||
-        _gameGrid[4] === _gameGrid[5] === _gameGrid[6] === 2 ||
-        _gameGrid[7] === _gameGrid[8] === _gameGrid[9] === 2 ||
-        _gameGrid[1] === _gameGrid[4] === _gameGrid[7] === 2 ||
-        _gameGrid[2] === _gameGrid[5] === _gameGrid[8] === 2 ||
-        _gameGrid[3] === _gameGrid[6] === _gameGrid[9] === 2 ||
-        _gameGrid[1] === _gameGrid[5] === _gameGrid[9] === 2 ||
-        _gameGrid[3] === _gameGrid[5] === _gameGrid[7] === 2 ) {
-            // Increment player2 wins by 1
-            player2.setWins(player2.getWins()+1);
-            return true;
+    const checkWin = () => {
+        if ((_gameGrid[1] === 'x' && _gameGrid[2] === 'x' && _gameGrid[3] === 'x') ||
+        (_gameGrid[4] === 'x' && _gameGrid[5] === 'x' && _gameGrid[6] === 'x') ||
+        (_gameGrid[7] === 'x' && _gameGrid[8] === 'x' && _gameGrid[9] === 'x') ||
+        (_gameGrid[1] === 'x' && _gameGrid[4] === 'x' && _gameGrid[7] === 'x') ||
+        (_gameGrid[2] === 'x' && _gameGrid[5] === 'x' && _gameGrid[8] === 'x') ||
+        (_gameGrid[3] === 'x' && _gameGrid[6] === 'x' && _gameGrid[9] === 'x') ||
+        (_gameGrid[1] === 'x' && _gameGrid[5] === 'x' && _gameGrid[9] === 'x') ||
+        (_gameGrid[3] === 'x' && _gameGrid[5] === 'x' && _gameGrid[7] === 'x') ) {
+            // return player 1 won
+            return 1;
+        } else if ((_gameGrid[1] === 'o' && _gameGrid[2] === 'o' && _gameGrid[3] === 'o') ||
+        (_gameGrid[4] === 'o' && _gameGrid[5] === 'o' && _gameGrid[6] === 'o') ||
+        (_gameGrid[7] === 'o' && _gameGrid[8] === 'o' && _gameGrid[9] === 'o') ||
+        (_gameGrid[1] === 'o' && _gameGrid[4] === 'o' && _gameGrid[7] === 'o') ||
+        (_gameGrid[2] === 'o' && _gameGrid[5] === 'o' && _gameGrid[8] === 'o') ||
+        (_gameGrid[3] === 'o' && _gameGrid[6] === 'o' && _gameGrid[9] === 'o') ||
+        (_gameGrid[1] === 'o' && _gameGrid[5] === 'o' && _gameGrid[9] === 'o') ||
+        (_gameGrid[3] === 'o' && _gameGrid[5] === 'o' && _gameGrid[7] === 'o') ) {
+            // return player 2 won
+            return 2;
+        } else if (_gameGrid[1] && _gameGrid[2] && _gameGrid[3] &&
+            _gameGrid[4] && _gameGrid[5] && _gameGrid[6] && _gameGrid[7] &&
+            _gameGrid[8] && _gameGrid[9] !== undefined) {
+            return 'tie';
         }
 
         return false;
     }
 
     const wipeBoard = () => {
+        // Empty the grid storage
         _gameGrid = [];
+        // Empty the text on the display
+        document.querySelectorAll('.boardSquare')
+                .forEach(element => element.textContent = '');
     }
 
     return { getSquare, setSquare, checkWin, wipeBoard };
@@ -73,34 +86,117 @@ const gameController = () => {
         target.classList.remove('hide');
     }
 
-    const _initializePlayers = (form, player1, player2) => {
-        
+    const _displayWinner = (player, playerNo, turnDisplay) => {
+        // Let the user know the game's over, and make text larger to emphasize
+        turnDisplay.textContent = `Game Over! ${player.getName()} WINS!!!`;
+        turnDisplay.classList.add('biggerDisplay');
+        /* Add bigWon class to the scoreboard indicators. Makes them green */
+        document.querySelectorAll(`.p${playerNo} .won`)
+                .forEach(element => element.classList.add('bigWon'));
+    }
+
+    const _updateWinner = (winner, winNum, turnDisplay) => {
+        // Display winner
+        turnDisplay.textContent = `${winner.getName()} WON!`;
+        // Update winner's wins attribute
+        winner.setWins(winner.getWins()+1);
+        // Update winner's scoreboard
+        document.querySelector(`.p${winNum} .indicator:nth-child(${winner.getWins()}`)
+                .classList.add('won');
+    }
+
+    const _newGame = (board, player1, player2, turnDisplay, playAgainButton, that) => {
+        playAgainButton.classList.add('hide');
+        board.wipeBoard();
+        let turn = [];
+        // Let player 1 know it's their turn
+        turnDisplay.textContent = `${player1.getName()}'s turn!`;
+        /* Remove all event listeners and add an even listener for each grid cell */
+        let boardSquare = document.querySelectorAll('.boardSquare');
+        boardSquare.forEach(element => element.replaceWith(element.cloneNode(true)));
+        /* Add an event listener for each grid cell 
+        (have to re-define because they were replaced) */
+        boardSquare = document.querySelectorAll('.boardSquare');
+        boardSquare.forEach(element => element.addEventListener('click', _playTurn.bind(null, turn, board, player1, player2, turnDisplay)));
+    }
+
+    const _initializePlayers = (form) => {
         /* Create a player object for both players, and use the names
         entered in the form */
-        player1 = playerFactory('x', form.elements['player1Name'].value)
-        player2 = playerFactory('o', form.elements['player2Name'].value)
+        const player1 = playerFactory('x', form.elements['player1Name'].value)
+        const player2 = playerFactory('o', form.elements['player2Name'].value)
 
         // Make a variable for the display of each player
         const p1Display = document.querySelector('.p1');
         const p2Display = document.querySelector('.p2');
-
+        
         /* Set the name displays to the player's names. */
         p1Display.querySelector('.name').textContent = player1.getName();
         p2Display.querySelector('.name').textContent = player2.getName();
-
+        
         // Remove the win class from all win indicators
         document.querySelectorAll('.won').forEach(element => element.classList.remove('won'));
-
+        
         // Display the player displays
         _unHideObject(p1Display);
         _unHideObject(p2Display);
+        
+        console.log(player2);
+        return [player1, player2];
     }
 
-    const runGame = () => {
+    const _playTurn = (turn, board, player1, player2, turnDisplay, that) => {
+        /* Target is the grid cell that was pressed's position */
+        let target = that.target.getAttribute('data-position');
+
+        /* Remove the event listeners from the cell once it's been pressed.
+        we do this by replacing the cell with a clone of itself */
+        that.target.replaceWith(that.target.cloneNode(true));
+
+        if (turn.length % 2 === 0) {
+            turnDisplay.textContent = `${player2.getName()}'s turn!`;
+            board.setSquare(target, player1.getSymbol());
+            turn.push(null);
+        } else {
+            turnDisplay.textContent = `${player1.getName()}'s turn!`;
+            board.setSquare(target, player2.getSymbol());
+            turn.push(null);
+        }
+
+        let winStatus = board.checkWin();
+
+        if (winStatus === 1) {
+            _updateWinner(player1, 1, turnDisplay);
+        } else if (winStatus === 2) {
+            _updateWinner(player2, 2, turnDisplay);
+        } else if (winStatus === 'tie') {
+            turnDisplay.textContent = 'Tie Game.';
+        }
+
+        if (player1.getWins() >= 5) {
+            _displayWinner(player1, 1, turnDisplay);
+            return;
+        } else if (player2.getWins() >= 5) {
+            _displayWinner(player2, 2, turnDisplay);
+            return;
+        }
+
+        // If the game is over
+        if (winStatus !== false) {
+            const playAgainButton = document.querySelector('.play-again');
+            // Display the play again button
+            playAgainButton.classList.remove('hide');
+            // And when it's pressed, start a new game.
+            playAgainButton.addEventListener('click', _newGame.bind(null, board, player1, player2, turnDisplay, playAgainButton));
+        }
+
+    }
+
+    const startGame = () => {
         const playButton = document.querySelector('.play.buttons');
         const form = document.querySelector('form');
-        const player1 = null;
-        const player2 = null;
+        const turnDisplay = document.querySelector('.turnIndicator');
+        let players = [];
         // Hide the button after it's been pressed
         _hideObject(playButton);
         _unHideObject(form);
@@ -109,20 +205,31 @@ const gameController = () => {
             // Stop from refreshing the page
             event.preventDefault();
             /* Generate the two players */
-            _initializePlayers(form, player1, player2)
-
+            players = _initializePlayers(form);
+            const player1 = players[0];
+            const player2 = players[1];
             //Reset the form and then hide it
             form.reset();
             _hideObject(form);
+
+            /* Generate a game board object and start a new game */
+            const board = gameBoard();
+            let turn = [];
+            // Let player 1 know it's their turn
+            turnDisplay.textContent = `${player1.getName()}'s turn!`;
+            /* Add an even listener for each grid cell */
+            document.querySelectorAll('.boardSquare')
+                .forEach(element => element.addEventListener('click', _playTurn.bind(null, turn, board, player1, player2, turnDisplay)));
         });
-
-
-
+        
     }
 
-    return {runGame};
+    return {startGame};
 }
 
-const gameInstance = gameController();
+// Keeps out of global scope
+(function() {
+    const gameInstance = gameController();
 
-document.querySelector('.play.buttons').addEventListener('click', gameInstance.runGame);
+    document.querySelector('.play.buttons').addEventListener('click', gameInstance.startGame);
+})()
